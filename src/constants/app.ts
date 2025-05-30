619707444acbb54c33426c3cb2e78985aeeb4593
NETKIA\antonio.bueno <antonio.bueno@netkia.es>
2025-05-30 13:45:10 +0000

    chore: Actualizar dependencias y variables de entorno

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -12,5 +12,5 @@ export const APP = {
   DEFAULT_LANGUAGE: 'es',
   SUPPORTED_LANGUAGES: ['es', 'en'] as const,
   APP_BASE_URL: env.APP_BASE_URL,
-  API_BASE_URL: typeof window === 'undefined' ? env.API_SERVER_BASE_URL : env.API_CLIENT_BASE_URL,
+  API_BASE_URL: env.API_BASE_URL,
 }
